a386959dca01d6838a13e4573769ed6599bf663c
snehajalan22 <72439072+snehajalan22@users.noreply.github.com>
2022-01-23 05:03:24 +0530

    Add files via upload

---
+++ components/Dietician.js
@@ -0,0 +1,124 @@
+import * as React from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, Image, TextInput, ScrollView} from 'react-native';
+import {Audio} from 'expo-av';
+import call from 'react-native-phone-call';
+
+
+
+class Dietician extends React.Component {    
+
+  constructor(){
+    super()
+    this.state={
+      text:"",
+      numbers:[]
+    }
+  }
+  triggerCall=()=>{
+    if(this.state.numbers.length != 10){
+    alert("Insert correct number")
+    }
+  }
+  
+ triggerCalling= (s) => {
+    // Check for perfect 10 digit length
+   
+    
+
+    const args = {
+      number: s,
+      prompt: true,
+    };
+    // Make a call
+    call(args).catch(console.error);
+  };
+
+
+   
+
+  goToHome=()=>{
+    this.props.navigation.navigate("appNav1")
+  }
+
+   goTocall=()=>{
+    this.props.navigation.navigate("appNav6")
+  }
+
+  goTomom=()=>{
+this.props.navigation.navigate("appNav3")
+  }
+
+  goTodad=()=>{
+this.props.navigation.navigate("appNav4")
+  }
+
+  render() {
+    return (
+      <ScrollView>
+       
+      
+
+
+<View style = {{ marginTop: 100, marginLeft: 80}}><Text style = {{fontSize: 48, fontWeight: "bold", fontFamily: 'sans-serif'}}>Dieticians</Text></View>
+
+      <View style= {{marginTop: -10, marginLeft: -20}}>
+      <TouchableOpacity style= {styling.Button} onPress={()=>{this.triggerCalling('9346065095')}}><Text>Hema Rathee</Text></TouchableOpacity></View>
+
+      <View style= {{marginTop: -10, marginLeft: -20}}>
+      <TouchableOpacity style= {styling.Button} onPress={()=>{this.triggerCalling('9940220005')}}><Text>Deepa Agarwal</Text></TouchableOpacity></View>
+
+      <View style= {{marginTop: -10, marginLeft: -20}}>
+      <TouchableOpacity style= {styling.Button}  onPress={()=>{this.triggerCalling('9703678826')}}><Text>Mahvash Fatima</Text></TouchableOpacity></View>
+
+      <View style= {{marginTop: -10, marginLeft: -20}}>
+      <TouchableOpacity style= {styling.Button}  onPress={()=>{this.triggerCalling('9885101292')}}><Text>Nutrition Freak</Text></TouchableOpacity></View>
+      
+
+       <View style= {{marginTop: -10, marginLeft: -20}}>
+      <TouchableOpacity style= {styling.Button} onPress={()=>{this.triggerCalling('092461 18485')}}><Text>Dr.Jyoti Chabria </Text></TouchableOpacity></View>
+ 
+      <TouchableOpacity style={{marginTop:20, marginLeft: 150, justifyContent:"center"}} onPress={this.goToHome}><Text>Back to Home</Text></TouchableOpacity>
+
+      </ScrollView>
+
+
+
+
+      
+    );
+  }
+}
+
+const styling = StyleSheet.create({
+Button:{
+  backgroundColor: "#CC9B92",
+  width: 200,
+  height: 60,
+  marginTop: 50,
+  marginLeft: 115,
+ alignItems: "center",
+ justifyContent: "center",
+ borderRadius: 200
+},
+
+Add:{
+backgroundColor: "#ffffed",
+  width: 300,
+  height: 40,
+  marginTop: 100,
+  marginLeft: 30,
+ alignItems: "center",
+ justifyContent: "center",
+ borderRadius: 200
+},
+
+
+textInputStyle:{
+marginTop: 10, marginLeft: 10, width: "80%", height: 40, alignSelf: "center", textAlign: "center", borderWidth: 1
+}
+
+
+
+})
+
+export default Dietician;
